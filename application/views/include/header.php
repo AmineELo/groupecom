7c4eae864fa3ac7548871c7a2ed2aa6502644801
frikhi123 <frikhihamza@gmail.com>
2016-04-23 18:48:41 +0100

    first upload

---
+++ application/views/include/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from aqvatarius.com/themes/taurus_v12/html/index.html by HTTrack Website Copier/3.x [XR&CO'2013], Fri, 22 Apr 2016 10:51:32 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+    <title>Groupe COM | Centre de formation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <link rel="icon" type="image/ico" href="favicon.html"/>
+
+    <link href="css/stylesheets.css" rel="stylesheet" type="text/css" />
+
+    <script type='text/javascript' src='js/plugins/jquery/jquery.min.js'></script>
+    <script type='text/javascript' src='js/plugins/jquery/jquery-ui.min.js'></script>
+    <script type='text/javascript' src='js/plugins/jquery/jquery-migrate.min.js'></script>
+    <script type='text/javascript' src='js/plugins/jquery/globalize.js'></script>
+    <script type='text/javascript' src='js/plugins/bootstrap/bootstrap.min.js'></script>
+
+    <script type='text/javascript' src='js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js'></script>
+    <script type='text/javascript' src='js/plugins/uniform/jquery.uniform.min.js'></script>
+
+    <script type='text/javascript' src='js/plugins/knob/jquery.knob.js'></script>
+    <script type='text/javascript' src='js/plugins/sparkline/jquery.sparkline.min.js'></script>
+    <script type='text/javascript' src='js/plugins/flot/jquery.flot.js'></script>
+    <script type='text/javascript' src='js/plugins/flot/jquery.flot.resize.js'></script>
+
+    <script type='text/javascript' src='js/plugins.js'></script>
+    <script type='text/javascript' src='js/actions.js'></script>
+    <script type='text/javascript' src='js/charts.js'></script>
+    <script type='text/javascript' src='js/settings.js'></script>
+
+</head>
+<body class="wall-num10" data-settings="open" style="">
+
+    <div class="container container- theme-black container-fixed">
---
+++ application/views/include/navigation.php
@@ -0,0 +1,53 @@
+<div class="row">
+           <div class="col-md-12">
+
+                <nav class="navbar brb" role="navigation">
+                   <div class="navbar-header">
+                       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                           <span class="sr-only">Toggle navigation</span>
+                           <span class="icon-reorder"></span>
+                       </button>
+                       <a class="navbar-brand" href="#"><img src="img/logo_gc_small.png"/></a>
+                   </div>
+                   <div class="collapse navbar-collapse navbar-ex1-collapse">
+                       <ul class="nav navbar-nav">
+                         <li class="active">
+                              <a href="#">
+                                  <span class="icon-home"></span> Accueil
+                              </a>
+                          </li>
+                          <li class="">
+                              <a href="#">
+                                  <span class="icon-info-sign"></span>A propos
+                              </a>
+                          </li>
+                          <li class="">
+                              <a href="#">
+                                  <span class="icon-envelope"></span>contact
+                              </a>
+                          </li>
+
+                           <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="icon-ellipsis-horizontal"></span> More</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Item 1</a></li>
+                                    <li><a href="#">Item 2</a></li>
+                                    <li><a href="#">Item 3</a></li>
+                                    <li><a href="#">Item 4</a></li>
+                                </ul>
+                            </li>
+
+                       </ul>
+                       <div class="nav navbar-nav  navbar-right">
+                            <li class="sep"></li>
+                             <li>
+                                  <a href="#" style="text-transform: capitalize;">
+                                      <span class="icon-signin"></span>Connectez-vous
+                                  </a>
+                              </li>
+                        </div>
+                   </div>
+               </nav>
+
+           </div>
+       </div>
