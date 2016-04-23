bd1e712433fa37e2506e0744fd59110da0da6216
frikhi123 <frikhihamza@gmail.com>
2016-04-23 23:00:06 +0100

    singn in

---
+++ application/controllers/Singnin.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Singnin extends CI_Controller {
+
+	public function index()
+	{
+        //Tester sur la session si il est remplie par les données de l'utilisateur
+       if($this->session->userdata('role'))
+         {
+          if($this->session->userdata('role')=="etudiant" || $this->session->userdata('role')=="ecole")
+          {
+            redirect('Accueil');
+          }
+         }
+        else
+         {
+         $this->load->view('singnin');
+         }
+	}
+
+	public function validation()
+	{
+		$this->form_validation->set_rules('username','Username','required|min_length[5]|max_length[30]');
+		$this->form_validation->set_rules('password','User password','required|min_length[5]|max_length[30]');
+
+			if($this->form_validation->run()==FALSE)
+				{
+					$val_errors=array('errors'=>validation_errors());
+					$this->session->set_flashdata($val_errors);
+					redirect('singnin');
+				}
+
+				else
+				{
+					$this->load->model('login_Model');
+					$username=$this->input->post('username');
+					$password=$this->input->post('password');
+					$tableau=$this->login_Model->login($username,$password);
+					var_dump($tableau);
+					if(empty($tableau))
+					{
+						$data_error['data_error']="Invalid Login, You should verify your data !";
+				  	 $this->session->set_flashdata($data_error);
+						redirect('singnin');
+					}
+					else
+					{
+						//Remplir notre session par les valeurs de l'utilisateur
+
+						$this->session->set_userdata($tableau[0]);
+						redirect('singnin');
+					}
+
+				}
+
+	}
+}
+?>
---
+++ application/models/Login_Model.php
@@ -0,0 +1,26 @@
+<?php
+  class Login_Model extends CI_Model
+  {
+    public function __construct()
+    {
+      parent::__construct();
+    }
+  	public function login($username,$password)
+  	{
+       $password=sha1($password);
+  		 $sql=$this->db->where(['username'=>$username,'password'=>$password])->get('users');//Notre requete
+        $donnes=$sql->result_array();//faire le resultat forme array de la requete dans une array
+        $vide=array();//array vide
+        //Tester si le nombre de ligne de requete retourne quelque chose ou rien
+        if($sql->num_rows() >=1)
+        {
+        return $donnes;
+        }
+        else
+        {
+        return $vide;
+        }
+  	}
+
+  }
+?>
